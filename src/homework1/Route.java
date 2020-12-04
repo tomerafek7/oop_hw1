d4eb2aea7dca6db37629beeb50dba915abd292e9
tomerafek7 <tomerafek7@gmail.com>
2020-12-04 11:47:02 +0200

    WIP Route.java

--- src/homework1/Route.java
+++ src/homework1/Route.java
@@ -1,6 +1,7 @@
 package homework1;
 
 import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
  * A Route is a path that traverses arbitrary GeoSegments, regardless
@@ -33,7 +34,17 @@ import java.util.Iterator;
  **/
 public class Route {
 
-	
+
+	final GeoPoint start, end;
+	final double startHeading, endHeading;
+	final ArrayList<GeoFeature> geoFeatures;
+	final ArrayList<GeoSegment> geoSegments;
+	final double length;
+	final GeoSegment endingGeoSegment;
+
+
+
+
  	// TODO Write abstraction function and representation invariant
 
 
@@ -47,16 +58,56 @@ public class Route {
      *          r.end = gs.p2
      **/
   	public Route(GeoSegment gs) {
-  		// TODO Implement this constructor
+  		assert gs != null : "Canont Create Route: the given GeoSegment = null";
+		this.start = gs.getP1();
+		this.end = gs.getP2();
+  		this.startHeading = gs.getHeading();
+  		this.endHeading = gs.getHeading();
+  		this.geoFeatures = new ArrayList<>();
+//  		this.geoFeatures.add()
+  		this.geoSegments = new ArrayList<>();
+		this.geoSegments.add(gs);
+		this.length = gs.getLength();
+		this.endingGeoSegment = gs;
   	}
 
+	/**
+	 * Constructs a new Route from an existing Route & A GeoSegment to append.
+	 * @requires r!=null && gs != null
+	 * @effects Constructs a new Route, r, such that
+	 *	        new_r.startHeading = r.startHeading &&
+	 *          new_r.endHeading = gs.heading &&
+	 *          new_r.start = r.start &&
+	 *          new_r.end = gs.p2
+	 **/
+	private Route(Route r, GeoSegment gs) {
+		assert gs != null && r != null : "Canont Create Route: one or more null parameters";
+		this.start = r.getStart();
+		this.end = gs.getP2();
+		this.startHeading = r.getStartHeading();
+		this.endHeading = gs.getHeading();
+		this.geoFeatures = new ArrayList<>();
+//  		this.geoFeatures.add()
+		this.geoSegments = new ArrayList<>();
+		this.endingGeoSegment = gs;
+
+		// running through all segments of r, and append them into the new route:
+		for (Iterator<GeoSegment> it = r.getGeoSegments(); it.hasNext(); ) {
+			GeoSegment seg = it.next();
+			this.geoSegments.add(seg);
+		}
+		// adding the last segment to the list:
+		this.geoSegments.add(gs);
+		// calc length:
+		this.length = r.length + gs.getLength();
+	}
 
     /**
      * Returns location of the start of the route.
      * @return location of the start of the route.
      **/
   	public GeoPoint getStart() {
-  		// TODO Implement this method
+  		return this.start;
   	}
 
 
@@ -65,7 +116,7 @@ public class Route {
      * @return location of the end of the route.
      **/
   	public GeoPoint getEnd() {
-  		// TODO Implement this method
+		return this.end;
   	}
 
 
@@ -75,7 +126,7 @@ public class Route {
    	 *         route, in degrees.
    	 **/
   	public double getStartHeading() {
-  		// TODO Implement this method
+  		return this.startHeading;
   	}
 
 
@@ -85,7 +136,7 @@ public class Route {
      *         route, in degrees.
      **/
   	public double getEndHeading() {
-  		// TODO Implement this method
+		return this.endHeading;
   	}
 
 
@@ -96,7 +147,7 @@ public class Route {
      *         traverse the route. These values are not necessarily equal.
    	 **/
   	public double getLength() {
-  		// TODO Implement this method
+  		return this.length;
   	}
 
 
@@ -110,8 +161,10 @@ public class Route {
      *         r.length = this.length + gs.length
      **/
   	public Route addSegment(GeoSegment gs) {
-  		// TODO Implement this method
-  	}
+  		assert gs != null: "Canont Create Route: the given GeoSegment = null";
+  		assert gs.getP1().equals(this.end): "Cannot Add Segment: gs.p1 != this.end";
+		return new Route(this, gs); // calling (private) C'tor.
+	}
 
 
     /**
